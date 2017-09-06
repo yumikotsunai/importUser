7b4aabbf735808a4b7c8abcab3b6c62d2c91cbfa
yumiko <yumiko-tsunai@kke.co.jp>
2017-09-06 02:15:19 +0000

    運用環境用のコールバックURLに変更

--- app/controllers/aouthsetup_controller.rb
+++ app/controllers/aouthsetup_controller.rb
@@ -16,8 +16,8 @@ CTX.verify_mode = OpenSSL::SSL::VERIFY_NONE
     @@client =	params[:clientId].presence
     @@secret =	params[:clientSecret].presence
     @@deviceId =	params[:deviceId].presence
-    @@callbackuri = 'https://import-user-yumikotsunai.c9users.io/aouthsetup/callback'
-    #@@callbackuri = 'https://importusers.herokuapp.com/aouthsetup/callback'
+    #@@callbackuri = 'https://import-user-yumikotsunai.c9users.io/aouthsetup/callback'
+    @@callbackuri = 'https://importusers.herokuapp.com/aouthsetup/callback'
     
     #connectのoauth認証のためのURLにアクセスする  (A)リソースオーナーにAuthorization Request送信
     req = 'https://connect.lockstate.jp/oauth/'+'authorize?'+'client_id='+@@client+'&response_type=code&redirect_uri='+@@callbackuri
